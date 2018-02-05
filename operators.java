af7f05495b5e0b3b36df9c4e5ae6a6959a012104
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-02-05 07:41:51 -0800

    Operators

---
+++ operators.java
@@ -0,0 +1,15 @@
+class Operators {
+    public static void main(String args[]) {
+//String name = "Merrell";
+//System.out.println(name);
+String firstName = "Frank";
+    String lastName = "Harvey";
+    String fullName = firstName + " " + lastName;
+
+    System.out.println(firstName);
+    System.out.println("firstName");
+    System.out.println(lastName);
+    System.out.println("lastName");
+    System.out.println(fullName);
+    System.out.println("fullName");  }
+}
