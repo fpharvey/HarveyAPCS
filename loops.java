413ea62ace08b19f1c5daa7412b92b7c746e0a34
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-02-26 07:27:36 -0800

    Ascii table loop

---
+++ loops.java
@@ -0,0 +1,9 @@
+class loops {
+    public static void main(String[] args){
+         char alpha=32;
+        for(int n=32;n<= 126;n++){
+            System.out.println(alpha);
+            alpha++; 
+        }
+    }
+}
