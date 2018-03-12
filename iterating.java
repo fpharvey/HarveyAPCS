eab334c7e0b2587ed7c1a83da092483961cf4392
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-03-12 07:40:43 -0700

    iterating

---
+++ iterating.java
@@ -0,0 +1,8 @@
+public class iterating{
+    public static void main(String [] args){
+         int[] nums = new int[201];
+         for(int i=1;i<nums.length;i++){
+             nums [0] =-100;
+             nums [i] =          }
+    }
+}
