5b6ae210413e57ae4d859fc9ddc309029036cc25
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-03-13 07:41:47 -0700

    hey

--- Assignments/iterating.java
+++ Assignments/iterating.java
@@ -1,8 +1,17 @@
 public class iterating{
     public static void main(String [] args){
-         int[] nums = new int[201];
-         for(int i=1;i<nums.length;i++){
-             nums [0] =-100;
-             nums [i] =          }
+        //int[] nums = new int[201];
+        //for(int i=1;i<nums.length;i++){
+            //nums [0] =-100;
+            //nums [i] =          
+            //}
+         if(Math.random()>.5){
+                    position ++;
+                }   
+                    else{
+                        position --; 
     }
+    System.out.print(position);
+    System.out.print(Math.random);
+}
 }
