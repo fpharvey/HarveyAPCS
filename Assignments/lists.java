540115704531476ff135dc1d0671b919b50db911
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-03-15 07:40:07 -0700

    To do list

---
+++ Assignments/lists.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class assignment{
+    public static void main(String[] Args){
+         ArrayList<Integer> numList = new ArrayList<Integer>();
+
+        //numList.add(5);
+        //numList.add(0,10);
+        //numList.add(1,15);
+        //numList.remove(1);
+        //numList.add(20);
+        //numList.add(1,25);
+        //numList.add(0, 30);
+        //numList.add(3, 35);
+        //numList.remove(2);
+        //numlist is 30 10 35 20
+
+    }
+}
---
+++ Projects/todolistapp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class todolist{
+    public static void main(String[] Args){
+        Arraylist<Integer> todolist = new Arraylist<Integer>();
+        System.out.println("Welcome to my todolist app");
+        System.out.print("T");
+        
+    }
+}
