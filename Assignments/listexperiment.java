fff66d499428f49bd17921d1843b8122300a2a93
Frank Harvey <fharvey43@gmail.com>
2018-03-25 14:42:03 -0700

    adds listexperiment

---
+++ Assignments/listexperiment.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class listexperiment{
+    public static ArrayList<String> hey = readFile(liststorage.java);
+    public static void main(String[] args){
+        hey.add("f");
+        hey.add("u");
+        for(int i = 0; i < hey.size();i ++){
+            System.out.print(hey.get(i));
+        }
+    }
+}
---
+++ Assignments/liststorage.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class liststorage{
+    public static ArrayList<String> hey = new ArrayList<String>();
+    public static void main(String [] args){
+        hey.add("c");
+    }
+}
