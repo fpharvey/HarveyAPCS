3e386267e558b3ca95ed5b2545b282d1110d976d
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-04-05 07:40:33 -0700

    hi

---
+++ Assignments/stringdecomp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class stringdecomp{
+    public static void main(String[] Args){
+        String s = "Given the circumstances, I'm not sure if it was a success, but it certainly seems promising.";
+        ArrayList<Integer> positionsofspaces= new ArrayList<Integer>();
+        positionsofspaces.add(23);
+        positionsofspaces.add(0,22);
+        System.out.print(positionsofspaces.get(0));
+    } 
+}
