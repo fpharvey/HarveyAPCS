255dbfd39b31ac4cfd48bd542acbaf0a0ab93c42
Frank Harvey <Harve.Fran2018@ssusd.org>
2018-04-16 07:40:39 -0700

    hi

---
+++ Assignments/2darray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class 2darray {
+    public static void main(String [] args){
+        int [][]matrix1 = [3][3]
+        for( int i = 0; i <matrix1.length; i ++){
+            for(int j = 0; j <matrix1[0].length; i ++){
+                if (i ==0){
+                    matrix1 [i][j] = 2math.pow(j+1);
+                }
+                if ( i==1){
+                    matrix1 [i][j] = 3math.pow(j+1);
+                }
+                 if ( i==2){
+                    matrix1 [i][j] = 5math.pow(j+1);
+                }
+        }
+    }
+}
